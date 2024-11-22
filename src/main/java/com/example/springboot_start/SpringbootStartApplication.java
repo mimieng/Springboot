6dedbbb35daa9c216fddc8defeab00719b873435
小邓c语言 <2900135517@qq.com>
2024-11-22 20:28:13 +0800

    web学习

--- src/main/java/com/example/springboot_start/SpringbootStartApplication.java
+++ src/main/java/com/example/springboot_start/SpringbootStartApplication.java
@@ -8,6 +8,7 @@ public class SpringbootStartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootStartApplication.class, args);
+
     }
 
 }
--- src/test/java/com/example/springboot_start/SpringbootStartApplicationTests.java
+++ src/test/java/com/example/springboot_start/SpringbootStartApplicationTests.java
@@ -11,3 +11,8 @@ class SpringbootStartApplicationTests {
     }
 
 }
+
+
+
+
+
